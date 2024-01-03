42d1123ea3ec1c3217f74e6c53825e6b4539417e
John Rei Enriquez <johnrei.enriquez@gmail.com>
2024-01-03 15:22:42 +0800

    added comment.nvim + oil improvements

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,15 @@
+return {
+  "numToStr/Comment.nvim",
+  opts = {
+    ignore = "^$",
+    toggler = {
+      line = "<leader>cc",
+      block = "<leader>bc",
+    },
+    opleader = {
+      line = "<leader>c",
+      block = "<leader>b",
+    },
+  },
+  lazy = false,
+}
